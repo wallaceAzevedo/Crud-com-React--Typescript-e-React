d8d97a4fcdcafb4f75093ce124a8e69cebac232a
Emma Paris <eparis@atlassian.com>
2021-10-08 14:17:47 -0300

    .

--- src/components/RecordError.tsx
+++ src/components/RecordError.tsx
@@ -1,12 +1,10 @@
 import { AxiosError } from 'axios';
-import { ValidationError } from 'class-validator';
 
 interface IProps {
   error: AxiosError;
 }
 
 export const RecordError: React.FC<IProps> =({ error }) =>{
-    const validationErrors = error?.response?.data
 
     return (
         <div className="error">
